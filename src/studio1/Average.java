0ae418281bc65ab4ccdd2cbc6a5d13d4042c4301
iniya.swaminathan <iniya.swaminathan@localhost>
2024-09-04 17:17:21 -0500

    Average!

---
+++ src/studio1/Average.java
@@ -0,0 +1,22 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Yo, sup?");
+		int n1 = in.nextInt();
+		System.out.println("The first of two integers to be averaged");
+		int n2 = in.nextInt();
+		
+		double average = n1+n2;
+		double number = average/2.0;
+		System.out.println(number);
+	}
+
+}
